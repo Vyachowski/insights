b5035ad070d5803760e278180ff6b8d209d0d567
Slava Haikin <vyachowski@gmail.com>
2026-01-30 10:35:59 +0300

    feat: add init template and update gitignore

--- src/init_db.js
+++ src/init_db.js
@@ -1,20 +1,31 @@
-import fs from 'fs';
-import path from 'path';
-import Database from 'better-sqlite3';
+// import path from 'path';
+// import Database from 'better-sqlite3';
 
-const initDb = () => {
-    const dbName = 'ses-statistics'
-    const dbFileExtension = 'sqlite3'
-    const dbFileName = `${dbName}.${dbFileExtension}`
-    const dbPath = path.resolve(process.cwd(), 'db', dbFileName)
-    
-    if (fs.existsSync(dbPath)) {
-        return null;
-    }
 
-    const db = new Database(dbPath);
 
-    if (db) {
-        return true;
-    }
-}
+
+// const initTable = (db) => {    
+//     db.exec(`
+//         CREATE TABLE IF NOT EXISTS Cities (
+//             id INTEGER PRIMARY KEY,
+//             name TEXT,
+//             alt_names TEXT,
+//             revenue_share NUMERIC,
+//             priority TEXT,
+//             display_order INTEGER
+//         );
+//     `)
+
+//     "Дата,Кто звонил,Откуда,№,Класс,Проект,Куда звонил,Запись,Комментарий,Вызов завершен"
+//     db.exec(`
+//         CREATE TABLE IF NOT EXISTS Calls (
+//             id INTEGER PRIMARY KEY,
+//             date TEXT,
+//             caller_phone TEXT,
+//             caller_region TEXT,
+//             priority TEXT,
+//             display_order INTEGER
+//         );
+//     `)
+// }
+
