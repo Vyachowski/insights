68b0a900b09bcfe0ec2459df348cb92885a21669
Slava Haikin <vyachowski@gmail.com>
2026-01-30 13:32:52 +0300

    feat: add prisma lib

---
+++ prisma.config.ts
@@ -0,0 +1,14 @@
+import "dotenv/config";
+import { defineConfig } from "prisma/config";
+
+const DATABASE_URL = `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}?schema=public`;
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  datasource: {
+    url: DATABASE_URL,
+  },
+});
