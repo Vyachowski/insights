ec1e9b4109da966b292217089c6e21cf108f0094
Slava Haikin <vyachowski@gmail.com>
2026-01-31 14:43:44 +0300

    feat: add counters parsing and update schema

---
+++ src/scripts/metrika-parsing/counter-parsing.ts
@@ -0,0 +1,21 @@
+import appRootPath from 'app-root-path';
+import fs from 'fs';
+import path from 'path';
+
+const data: Record<string, string> = JSON.parse(fs.readFileSync(path.resolve(appRootPath.path, 'data/metrika/counters/counters.json'), 'utf8'));
+
+if (data && Array.isArray(data.counters)) {
+    const rows = data.counters.map(c =>
+    `"${c.name.replace(/"/g, '""')}",${c.id},"${c.site}"`
+    );
+
+    const csv = [
+    'name,id,site',
+    ...rows
+    ].join('\n');
+
+    fs.writeFileSync('counters.csv', csv, 'utf8');
+
+    console.log('CSV готов');
+}
+
