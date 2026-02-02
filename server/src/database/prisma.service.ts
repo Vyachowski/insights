c95d2f38343752f31d160d7385867c681a0a8103
Slava Haikin <vyachowski@gmail.com>
2026-02-02 17:29:09 +0300

    [server]feat: add prisma service

---
+++ server/src/database/prisma.service.ts
@@ -0,0 +1,25 @@
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../../generated/prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor(private configService: ConfigService) {
+    const connectionString = configService.get<string>('DB_URL');
+    const adapter = new PrismaPg({ connectionString });
+
+    super({ adapter });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
