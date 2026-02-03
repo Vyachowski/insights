6d5571a3a76a864eed49045c77f413860e81fc69
Slava Haikin <vyachowski@gmail.com>
2026-02-03 10:59:19 +0300

    feat: add db module with proper configuration

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { validate } from './config/config.validation';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { validate } from './config/config.validation';
       isGlobal: true,
       validate,
     }),
+    DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
