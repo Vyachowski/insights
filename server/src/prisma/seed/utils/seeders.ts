df1063eea21df3d765bd42083a320a5804bcabab
Slava Haikin <vyachowski@gmail.com>
2026-02-03 13:05:37 +0300

    [server]fix: add correct db connection string

--- server/src/prisma/seed/utils/seeders.ts
+++ server/src/prisma/seed/utils/seeders.ts
@@ -10,7 +10,7 @@ import {
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from 'generated/prisma/client';
 
-const pool = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const pool = new PrismaPg({ connectionString: process.env.DB_URL });
 const prisma = new PrismaClient({ adapter: pool });
 
 export async function seedCities(citiesPath: string): Promise<void> {
