1d9f6ef282798b0a5efaa8bc9942e7cbe35b1109
Slava Haikin <vyachowski@gmail.com>
2026-02-03 12:35:59 +0300

    [server]fix: update env schema and connection string env

--- server/src/config/validation.config.ts
+++ server/src/config/validation.config.ts
@@ -4,7 +4,6 @@ const envSchema = z.object({
   NODE_ENV: z
     .enum(['development', 'production', 'test'])
     .default('development'),
-  PORT: z.coerce.number().default(3000),
   DB_URL: z.url(),
 });
 
--- server/src/prisma/seed/utils/checkers.ts
+++ server/src/prisma/seed/utils/checkers.ts
@@ -3,7 +3,7 @@ import fs from 'fs/promises';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from 'generated/prisma/client';
 
-const pool = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const pool = new PrismaPg({ connectionString: process.env.DB_URL });
 const prisma = new PrismaClient({ adapter: pool });
 
 export async function checkFilesExistence(paths: string[]): Promise<void> {
