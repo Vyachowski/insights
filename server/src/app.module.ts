720463d96db25b21df960333fff39614b0bb7da5
Slava Haikin <vyachowski@gmail.com>
2026-02-04 09:35:15 +0300

    [server]feat: add auth template

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { validate } from './config/validation.config';
 import { DatabaseModule } from './database/database.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { DatabaseModule } from './database/database.module';
       validate,
     }),
     DatabaseModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ server/src/auth/auth.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Get()
+  getProfile() {}
+
+  @Post()
+  createUser(login: string, password: string) {}
+}
