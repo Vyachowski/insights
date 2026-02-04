b22030fc45cf8ee31b4e1061c3787ab830b27b44
Slava Haikin <vyachowski@gmail.com>
2026-02-04 16:26:29 +0300

    [server]feat: add profile method

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,17 +1,37 @@
-import { Controller, Get, UseGuards, Request } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  UseGuards,
+  Request,
+  NotFoundException,
+} from '@nestjs/common';
 import { AppService } from './app.service';
-import { User } from 'generated/prisma/browser';
 import { LoginRequestDto } from './auth/dto/auth.dto';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { PrismaService } from './database/prisma.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly prismaService: PrismaService,
+  ) {}
 
   @UseGuards(JwtAuthGuard)
   @Get('profile')
-  getProfile(@Request() req: LoginRequestDto & { user: User }) {
-    return req.user;
+  async getProfile(
+    @Request() req: LoginRequestDto & { user: { userId: string } },
+  ) {
+    const user = await this.prismaService.user.findUnique({
+      where: { id: req.user.userId },
+    });
+
+    if (!user) throw new NotFoundException('User was not found.');
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { password: _, ...userWithoutPassword } = user;
+
+    return userWithoutPassword;
   }
 
   @Get()
