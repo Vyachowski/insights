a8d4a74442bdea200398789a1b307710efb000c6
Slava Haikin <vyachowski@gmail.com>
2026-02-04 13:29:49 +0300

    [server]feat: move profile controller to to app module

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,9 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards, Request } from '@nestjs/common';
 import { AppService } from './app.service';
+import { User } from 'generated/prisma/browser';
+import { LoginRequestDto } from './auth/dto/auth.dto';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req: LoginRequestDto & { user: User }) {
+    return req.user;
+  }
 
   @Get()
   getHello(): string {
--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,26 +1,12 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Post,
-  Request,
-  UseGuards,
-} from '@nestjs/common';
+import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginRequestDto } from './dto/auth.dto';
 import { LocalAuthGuard } from './local.auth.guard';
 import { User } from 'generated/prisma/client';
-import { JwtAuthGuard } from './jwt-auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-
-  @UseGuards(JwtAuthGuard)
-  @Get('profile')
-  getProfile(@Request() req: LoginRequestDto & { user: User }) {
-    return req.user;
-  }
 
   @UseGuards(LocalAuthGuard)
   @Post('login')
