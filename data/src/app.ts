1aae1599882b472a876a8851e20d9ff7a7077971
Slava Haikin <vyachowski@gmail.com>
2026-02-11 16:24:39 +0300

    [data]chore: delete site metrics option

--- data/src/app.ts
+++ data/src/app.ts
@@ -35,7 +35,7 @@ async function app(logger = console.log) {
   const calls = await createCallsCSV(cities.data);
   logger(calls.message, breaker);
 
-  const metrics = await createSiteMetricsCSV(sites.data, { source: "backup" });
+  const metrics = await createSiteMetricsCSV(sites.data);
   logger(metrics.message, breaker);
 
   const revenue = await createRevenueCSV();
