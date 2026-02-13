0bbeaf7593f7311dbdfe02943e5d2b241fc50d91
Slava Haikin <vyachowski@gmail.com>
2026-02-13 23:02:10 +0300

    [server]chore: add unused vars linter rule

--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -26,10 +26,18 @@ export default tseslint.config(
   },
   {
     rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_$',
+          varsIgnorePattern: '^_$',
+          caughtErrorsIgnorePattern: '^_$',
+        },
+      ],
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
-      "prettier/prettier": ["error", { endOfLine: "auto" }],
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
     },
   },
 );
