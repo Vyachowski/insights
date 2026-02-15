14473da4fef760f69b55d82a6454830b2d44c52d
Slava Haikin <vyachowski@gmail.com>
2026-02-15 20:57:59 +0300

    [server]feat: add cors policy

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const isDev = process.env.NODE_ENV !== 'production';
 
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(
@@ -13,6 +14,18 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+  app.enableCors({
+    origin: isDev
+      ? {
+          origin: true,
+          credentials: true,
+        }
+      : {
+          origin: process.env.DOMAIN,
+          credentials: true,
+        },
+    credentials: true,
+  });
   app.use(cookieParser());
 
   await app.listen(process.env.PORT ?? 3000);
