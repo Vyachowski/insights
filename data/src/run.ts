98e962656044fbdcc1265ded5ec74675de898ecc
Slava Haikin <vyachowski@gmail.com>
2026-02-21 19:07:07 +0300

    [data]chore: move comment away from the code

--- data/src/run.ts
+++ data/src/run.ts
@@ -1,10 +1,11 @@
+import type { RunParams } from "./types";
+
 import { createCallsCSV } from "./creators/calls/main";
 import { createCitiesCSV } from "./creators/create-cities";
 import { createRevenueCSV } from "./creators/create-revenue";
 import { createSitesCSV } from "./creators/create-sites";
 import { createExpensesCSV } from "./creators/expenses/main";
 import { createSiteMetricsCSV } from "./creators/site-metrics/main";
-import type { RunParams } from "./types";
 
 export default async function run({ inputPaths, outputPaths, outputDir, startDate, endDate, options }: RunParams) {
   const {
@@ -25,13 +26,6 @@ export default async function run({ inputPaths, outputPaths, outputDir, startDat
   logger(calls.message, breaker);
 
   const metrics = await createSiteMetricsCSV({ inputPath: inputPaths.siteMetrics, outputPath: outputPaths.siteMetrics, sites: sites.data, options: { source: "backup" }});
-  // FIXME: Fix this function to correctly process data
-  // const metrics = await createSiteMetricsCSV(sites.data, {
-  //   source: "api",
-  //   startDate: new Date(config.IMPORT_START_DATE),
-  //   endDate: new Date(config.IMPORT_END_DATE),
-  //   years: [2022],
-  // });
   logger(metrics.message, breaker);
 
   const revenue = await createRevenueCSV({ inputPath: inputPaths.revenue, outputPath: outputPaths.revenue });
@@ -40,3 +34,11 @@ export default async function run({ inputPaths, outputPaths, outputDir, startDat
   const expenses = await createExpensesCSV({ outputPath: outputPaths.expenses, cities: cities.data, startDate, endDate });
   logger(expenses.message, breaker);
 }
+
+// NOTE: Next type call signature
+// const metrics = await createSiteMetricsCSV(sites.data, {
+//   source: "api",
+//   startDate: new Date(config.IMPORT_START_DATE),
+//   endDate: new Date(config.IMPORT_END_DATE),
+//   years: [2022],
+// });
