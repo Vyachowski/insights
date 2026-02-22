a1b842f3f165f8eda75714a7f7d52d8edd954618
Slava Haikin <vyachowski@gmail.com>
2026-02-22 14:02:40 +0300

    [server]chore: reorganize function calls

--- server/src/prisma/seed/seed.ts
+++ server/src/prisma/seed/seed.ts
@@ -2,8 +2,8 @@ import seed from './modules/main';
 import getSharedConfig from '@shared/config';
 import { defineRepoRootPath, resolveSharedPaths } from '@shared/utils';
 
-const { FILE_PREFIX } = getSharedConfig();
 const rootPath = defineRepoRootPath();
+const { FILE_PREFIX } = getSharedConfig();
 const { filePaths: paths } = resolveSharedPaths(rootPath, FILE_PREFIX);
 
 seed(paths).catch((e) => {
