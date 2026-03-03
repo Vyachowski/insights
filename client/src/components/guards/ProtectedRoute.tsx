912c3a3d735ae07bf9d7b6fb971b6776ae7ad652
Slava Haikin <vyachowski@gmail.com>
2026-03-03 20:12:47 +0300

    [client]feat: add error screen for protected layout

--- client/src/components/guards/ProtectedRoute.tsx
+++ client/src/components/guards/ProtectedRoute.tsx
@@ -1,12 +1,13 @@
 import { useEffect } from 'react'
 import { Navigate, Outlet } from 'react-router'
 
+import ErrorScreen from '../screens/ErrorScreen'
 import SplashScreen from '../screens/SplashScreen'
 
 import { useAuth } from '@/hooks/useAuth'
 
 export default function ProtectedRoute() {
-  const { isAuthenticated, isLoading, isLoaded, checkAuth } = useAuth()
+  const { isAuthenticated, isLoading, isLoaded, checkAuth, error } = useAuth()
 
   const isAuthChecking = !isLoaded || isLoading
   const isLoggedOut = !isAuthenticated && isLoaded
@@ -18,6 +19,7 @@ export default function ProtectedRoute() {
   }, [isAuthenticated, checkAuth])
 
   if (isAuthChecking) return <SplashScreen />
+  if (error) return <ErrorScreen error={error} />
   if (isLoggedOut) return <Navigate to="/login" replace />
 
   return <Outlet />
