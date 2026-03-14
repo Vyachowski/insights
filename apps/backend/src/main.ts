9ffe9324e2cd572d6c6f7557ff61cd848b4b2714
Slava Haikin <slava@Slavas-MacBook-Air.local>
2026-03-14 13:41:26 +0300

    [server]chore: add correct app config and add env validation

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -2,10 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const isDev = process.env.NODE_ENV !== 'production';
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const configService = app.get(ConfigService);
+
+  const isDev = configService.get('NODE_ENV') !== 'production';
+  const port = configService.get<number>('PORT') ?? 3000;
+  const allowedOrigin = configService.get<string>('ALLOWED_ORIGIN');
 
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(
@@ -15,12 +21,12 @@ async function bootstrap() {
     }),
   );
   app.enableCors({
-    origin: isDev ? true : process.env.DOMAIN,
+    origin: isDev ? true : allowedOrigin,
     credentials: true,
   });
   app.use(cookieParser());
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(port);
 }
 
 void bootstrap();
