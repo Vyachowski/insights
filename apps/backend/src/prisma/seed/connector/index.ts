e139de6104c95523d0cac65819277887f357ebe1
Slava Haikin <slava@Slavas-MacBook-Air.local>
2026-03-14 17:30:22 +0300

    [backend]fix: add prod db url

--- apps/backend/src/prisma/seed/connector/index.ts
+++ apps/backend/src/prisma/seed/connector/index.ts
@@ -1,6 +1,11 @@
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@/prisma/generated/prisma/client';
 
-const pool = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const isProd = process.env['NODE_ENV'] === 'production';
+const pool = new PrismaPg({
+  connectionString: isProd
+    ? process.env.DATABASE_PUBLIC_URL
+    : process.env.DATABASE_URL,
+});
 
 export const prisma = new PrismaClient({ adapter: pool });
